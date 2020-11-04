403297e52820dc7b8f926b6a2c424b82066e3ef0
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2020-11-04 14:49:37 +1100

    Variation and small edits

---
+++ shared/navbar.js
@@ -0,0 +1,27 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+let navbar = (props) => {
+
+    return(
+        
+        <Navbar varient="light" expand="sm" style={{paddingTop: '22px', paddingBottom:'22px'}}>
+        <Navbar.Brand href="/">Presence</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Nav className="justify-content-end"  activeKey={props.activeLink}>
+            <Nav.Link href="/articles/all">Articles</Nav.Link>
+            <Nav.Link href="#link">Contact Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      
+    )
+}
+
+export default navbar;
