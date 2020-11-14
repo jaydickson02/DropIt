02304fefd992e9aff17ee68942c676dc3c933b9a
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2020-11-14 21:23:43 +1100

    Added New device form

---
+++ pages/newdevice.js
@@ -0,0 +1,52 @@
+import Layout from '../shared/layout'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Container } from 'react-bootstrap'
+
+const index = () => {
+
+
+return(
+<Layout activeLink={'/'}>
+<Container>
+<Form action='https://drop-it-db.vercel.app/api/add' method='post'>
+  <Form.Group controlId="formDeviceType">
+    <Form.Label>Device Type</Form.Label>
+    <Form.Control as="select">
+      <option>Chromebook</option>
+      <option>iPad</option>
+      <option>Laptop</option>
+      <option>Monitor</option>
+      <option>Other</option>
+    </Form.Control>
+  </Form.Group>
+
+  <Form.Group controlId="formDeviceSerialNumber">
+    <Form.Label>Serial Number</Form.Label>
+    <Form.Control type="text" placeholder="Serial Number" />
+  </Form.Group>
+
+  <Form.Group controlId="formCurrentDeviceUser">
+    <Form.Label>Current User</Form.Label>
+    <Form.Control type="text" placeholder="Ex. Jane Doe" />
+  </Form.Group>
+
+  <Form.Group controlId="formDeviceCondition">
+    <Form.Label>Condition</Form.Label>
+    <Form.Control as="select">
+      <option>Working</option>
+      <option>Repairing</option>
+      <option>Broken</option>
+    </Form.Control>
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+</Container>
+</Layout>
+)
+}
+
+export default index
