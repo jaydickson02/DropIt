5a5456a165b63404385cab350cb983f6167ed8ff
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2020-11-22 21:14:11 +1100

    Add a basic landing page

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,38 @@
+import { Container, Alert, Jumbotron, Button } from 'react-bootstrap'
 import Layout from '../shared/layout'
 
 const index = (props) => {
     console.log(props)
 return(
 <Layout activeLink={'/'}>
-    {props.devices.map(device => <div><a href={'/devices/' + device.serialNumber}>{device.serialNumber}</a></div>) }
+    <Container>
+
+    <hr />
+
+        <Alert variant={'primary'}>
+            This is the landing page for DropIT. Welcome. We're currently under construction. Feel free to have a look around!
+        </Alert>
+
+    <hr />
+
+        <Jumbotron>
+            <h1>DropIt</h1>
+            <p>
+                We're working to build a clean, minimal and highly functional system thats capable of managing all the needs of an IT Department with respect to hardware. Soloutions are available for inventory organisation, tracking repairs and managing users.
+            </p>
+
+            <p>
+                Click below to check out the devices page. 
+            </p>
+                
+            
+            <p>
+                <Button href="/devices" variant="primary">Go</Button>
+            </p>
+        </Jumbotron>
+    </Container>
 </Layout>
 )
 }
-
-index.getInitialProps = async (ctx) => {
-    const res = await fetch('https://drop-it-db.vercel.app/api/devices')
-    const json = await res.json()
-    return { devices: json }
-  }
 
 export default index
