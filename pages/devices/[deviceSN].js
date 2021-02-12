26732b4032a239a734229a9b3000590350b01d22
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2021-02-13 00:52:38 +1100

    Name error index is not defined

--- pages/devices/[deviceSN].js
+++ pages/devices/[deviceSN].js
@@ -33,7 +33,7 @@ return(
 )
 }
 
-index.getInitialProps = async (ctx) => {
+device.getInitialProps = async (ctx) => {
     const res = await fetch('https://drop-it-db.vercel.app/api/devices')
     const json = await res.json()
     return { devices: json }
